2b3aba7d1d00682958427ae8e6b483916f861674
Виталий Чухляев <vitaliy69@mail.ru>
2022-12-30 12:33:04 +0300

    Added new route name='articles' in urls.py

--- m2m_relations/urls.py
+++ m2m_relations/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from articles.views import articles_list
 
 urlpatterns = [
+    path('', articles_list, name='articles'),
     path('admin/', admin.site.urls),
 ]
