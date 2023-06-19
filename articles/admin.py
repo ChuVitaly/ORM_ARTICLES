d7d2c3259793c4f88160df29021fd5c4166a655a
Виталий Чухляев <vitaliy69@mail.ru>
2023-06-19 22:59:21 +0300

    Added check for main partition

--- articles/admin.py
+++ articles/admin.py
@@ -1,15 +1,31 @@
+from django.forms import BaseInlineFormSet, ValidationError
 from django.contrib import admin
-
 from .models import Article, Comment, Tag, Scope, ArticleTags
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_scope_count = 0
+        for form in self.forms:
+            cleaned_data = form.cleaned_data
+            if cleaned_data.get('is_main'):
+                main_scope_count += 1
+
+        if main_scope_count != 1:
+            raise ValidationError('Ошибка: должен быть указан один основной раздел.')
+
+        return super().clean()
 
 
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = RelationshipInlineFormset
     extra = 1
     verbose_name_plural = 'Разделы'
     verbose_name = 'Раздел'
     fields = ['tag', 'is_main']
     ordering = ['is_main', 'tag__name']
+
 
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ScopeInline, ]
@@ -29,13 +45,3 @@ admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(ArticleTags)
-
-# @admin.register(Article)
-# class ArticleAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'text', 'published_at', 'image')
-
-
-
-
-
-
